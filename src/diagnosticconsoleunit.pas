20757cd1fb7bd59a33251d5a6069fe6805a39834
markfirmware <markfirmware@users.noreply.github.com>
2017-06-23 19:18:08 -0400

    format

--- src/diagnosticconsoleunit.pas
+++ src/diagnosticconsoleunit.pas
@@ -2,9 +2,11 @@ unit DiagnosticConsoleUnit;
 {$mode delphi}{$h+}
 
 interface
-procedure DiagnosticConsoleLog(S:String);
+
+procedure DiagnosticConsoleLog(const S: String);
 
 implementation
+
 uses
  QEMUVersatilePB,VersatilePb,
  Classes,Console,GlobalConfig,GlobalConst,GlobalTypes,
@@ -12,46 +14,46 @@ uses
  Services,SysUtils,Threads,Ultibo;
 
 var
- ScopeWindow,MessagesWindow:TWindowHandle;
+ ScopeWindow, MessagesWindow: TWindowHandle;
 
-procedure DiagnosticConsoleLog(S:String);
+procedure DiagnosticConsoleLog(const S: String);
 begin
- LoggingOutput(S);
- ConsoleWindowWriteLn(MessagesWindow,S);
+  LoggingOutput(S);
+  ConsoleWindowWriteLn(MessagesWindow,S);
 end;
 
 procedure StartLogging;
 begin
- LOGGING_INCLUDE_COUNTER:=False;
- LOGGING_INCLUDE_TICKCOUNT:=True;
- SERIAL_REGISTER_LOGGING:=True;
- SerialLoggingDeviceAdd(SerialDeviceGetDefault);
- SERIAL_REGISTER_LOGGING:=False;
- LoggingDeviceSetDefault(LoggingDeviceFindByType(LOGGING_TYPE_SERIAL));
+  LOGGING_INCLUDE_COUNTER := False;
+  LOGGING_INCLUDE_TICKCOUNT := True;
+  SERIAL_REGISTER_LOGGING := True;
+  SerialLoggingDeviceAdd(SerialDeviceGetDefault);
+  SERIAL_REGISTER_LOGGING := False;
+  LoggingDeviceSetDefault(LoggingDeviceFindByType(LOGGING_TYPE_SERIAL));
 end;
 
 const
  HEAP_FLAG_SYSTEM_DIAGNOSTIC=$08000000;
 
 type
- PSystemDiagnosticStorage = ^TSystemDiagnosticStorage;
- TSystemDiagnosticStorage = packed record
-  ReservedForHeapManager:array[0..31] of Byte;
-  Signature:LongWord;
-  MostRecentClockCount:LongWord;
-  TotalFree:LongWord;
-  TerminatedThreadCount:LongWord;
- end;
+  PSystemDiagnosticStorage = ^TSystemDiagnosticStorage;
+  TSystemDiagnosticStorage = packed record
+    ReservedForHeapManager:array[0..31] of Byte;
+    Signature:LongWord;
+    MostRecentClockCount:LongWord;
+    TotalFree:LongWord;
+    TerminatedThreadCount:LongWord;
+  end;
 
- TSystemDiagnostic = record
-  Valid:Boolean;
-  Storage:PSystemDiagnosticStorage;
-  constructor Create(Where:Pointer);
-  procedure Update;
- end;
+  TSystemDiagnostic = record
+    Valid:Boolean;
+    Storage:PSystemDiagnosticStorage;
+    constructor Create(Where:Pointer);
+    procedure Update;
+  end;
 
 var
- SystemDiagnostic:TSystemDiagnostic;
+  SystemDiagnostic:TSystemDiagnostic;
 
 constructor TSystemDiagnostic.Create(Where:Pointer);
 const
@@ -72,78 +74,85 @@ begin
 end;
 
 const
- ThreadIndices=10;
- ThreadStateIndices=10;
+ ThreadIndices = 10;
+ ThreadStateIndices = 10;
 
 function ThreadIndex(Name:String):LongWord;
 begin
- if Name = 'TxThread' then
-   Result:=1
- else if Name = 'TCP Listener' then
-   Result:=2
- else if Name = 'TCP Server' then
-   Result:=3
- else
-   Result:=0;
+  if Name = 'TXThread' then
+    Result:=1
+  else if Name = 'TCP Listener' then
+    Result:=2
+  else if Name = 'TCP Server' then
+    Result:=3
+  else
+    Result:=0;
 end;
 
+type
+  TThreadStates = array[0..ThreadIndices, 0..ThreadStateIndices] of LongWord;
+
 procedure TSystemDiagnostic.Update;
-var
- HeapStatus:THeapStatus;
- ThreadSnapShot,Current:PThreadSnapShot;
- ThreadStates:array[0..ThreadIndices,0..ThreadStateIndices] of LongWord;
- I,J:Integer;
- procedure EndLine;
- begin
-  ConsoleClrEol;
-  ConsoleGotoXY(1,ConsoleWhereY + 1);
- end;
- procedure Line(S:String);
- begin
-  Write(S);
-  EndLine;
- end;
- procedure WriteStates(ThreadIndex:Integer;Name:String);
- var
-  J:Integer;
- begin
-  Write(Format('   %12s ',[Name]));
-  for J:=0 to ThreadStateIndices do
-   Write(Format(' %3d',[ThreadStates[ThreadIndex,J]]));
-  EndLine;
- end;
-begin
- if Valid then
+
+  procedure EndLine;
   begin
-   ConsoleGotoXY(1,1);
-   HeapStatus:=GetHeapStatus;
-   Storage.MostRecentClockCount:=ClockGetCount;
-   Storage.TotalFree:=HeapStatus.TotalFree;
-   Storage.TerminatedThreadCount:=0;
-   for I:=0 to ThreadIndices do
-    for J:=0 to ThreadStateIndices do
-     ThreadStates[I,J]:=0;
-   ThreadSnapShot:=ThreadSnapShotCreate;
-   Current:=ThreadSnapShot;
-   while Assigned(Current) do
-    begin
-     if Current.State = THREAD_STATE_TERMINATED then
-      Inc(Storage.TerminatedThreadCount);
-     Inc(ThreadStates[ThreadIndex(Current.Name),Current.State]);
-     Current:=Current.Next;
-    end;
-   ThreadSnapShotDestroy(ThreadSnapShot);
-   Line(Format('   Up Time %s',[FileTimeToSysLogDateTime(UpTime)]));
-   Line(Format('   THeapstatus.TotalFree %8d Terminated Threads %3d',[Storage.TotalFree,Storage.TerminatedThreadCount]));
-   WriteStates(1,'TxThread');
-   WriteStates(2,'TCP Listener');
-   WriteStates(3,'TCP Server');
+    ConsoleClrEol;
+    ConsoleGotoXY(1, ConsoleWhereY + 1);
   end;
+
+  procedure Line(S: String);
+  begin
+    Write(S);
+    EndLine;
+  end;
+
+  procedure WriteStates(var ThreadStates: TThreadStates; ThreadIndex:Integer; Name:String);
+  var
+    J: Integer;
+  begin
+    Write(Format('   %12s ', [Name]));
+    for J := 0 to ThreadStateIndices do
+      Write(Format(' %3d', [ThreadStates[ThreadIndex, J]]));
+    EndLine;
+  end;
+
+var
+  HeapStatus:THeapStatus;
+  I, J: Integer;
+  ThreadSnapShot, Current: PThreadSnapShot;
+  ThreadStates: TThreadStates;
+begin
+  if not Valid then
+    Exit;
+  ConsoleGotoXY(1,1);
+  HeapStatus:=GetHeapStatus;
+  Storage.MostRecentClockCount:=ClockGetCount;
+  Storage.TotalFree:=HeapStatus.TotalFree;
+  Storage.TerminatedThreadCount:=0;
+  FillChar(ThreadStates, SizeOf(ThreadStates), 0);
+  ThreadSnapShot := ThreadSnapShotCreate;
+  try
+    Current := ThreadSnapShot;
+    while Current <> nil do
+    begin
+      if Current.State = THREAD_STATE_TERMINATED then
+        Inc(Storage.TerminatedThreadCount);
+      Inc(ThreadStates[ThreadIndex(Current.Name), Current.State]);
+      Current := Current.Next;
+    end;
+  finally
+    ThreadSnapShotDestroy(ThreadSnapShot);
+  end;
+  Line(Format('   Up Time %s', [FileTimeToSysLogDateTime(UpTime)]));
+  Line(Format('   THeapstatus.TotalFree %8d Terminated Threads %3d', [Storage.TotalFree,Storage.TerminatedThreadCount]));
+  WriteStates(ThreadStates, 1, 'TXThread');
+  WriteStates(ThreadStates, 2, 'TCP Listener');
+  WriteStates(ThreadStates, 3, 'TCP Server');
 end;
 
 procedure DiagnosticConsoleUpdate;
 begin
- SystemDiagnostic.Update;
+  SystemDiagnostic.Update;
 end;
 
 const
@@ -152,47 +161,48 @@ const
 var
  IdleThread:TThreadHandle;
 
-function IdleThreadProcedure(Parameter:Pointer):PtrInt;
+function IdleThreadProcedure(Parameter: Pointer): PtrInt;
 begin
  Result:=0;
- ThreadSetName(GetCurrentThreadId,'IdleThread');
+ ThreadSetName(GetCurrentThreadId, 'IdleThread');
  while True do
   begin
-   PLongWord(VERSATILEPB_UART1_REGS_BASE)^:=Ord('a');
+   PLongWord(VERSATILEPB_UART1_REGS_BASE)^ := Ord('a');
    Sleep(1*1000);
   end;
 end;
 
 var
- ScopeThread:TThreadHandle;
+ ScopeThread: TThreadHandle;
 
-function ScopeThreadProcedure(Parameter:Pointer):PtrInt;
+function ScopeThreadProcedure(Parameter: Pointer): PtrInt;
 begin
- Result:=0;
- ThreadSetName(GetCurrentThreadId,'ScopeThread');
- while True do
+  Result:=0;
+  ThreadSetName(GetCurrentThreadId, 'ScopeThread');
+  while True do
   begin
-   SystemDiagnostic.Update;
-   Sleep(200);
+    SystemDiagnostic.Update;
+    Sleep(200);
   end;
 end;
 
 procedure InitDiagnosticConsole;
 begin
- SystemDiagnostic:=TSystemDiagnostic.Create(Pointer(DiagnosticPointer));
- IdleThread:=BeginThread(@IdleThreadProcedure,nil,IdleThread,THREAD_STACK_DEFAULT_SIZE);
- ScopeWindow:=ConsoleWindowCreate(ConsoleDeviceGetDefault,CONSOLE_POSITION_TOP,True);
- ConsoleWindowSetBackColor(ScopeWindow,COLOR_ORANGE);
- MessagesWindow:=ConsoleWindowCreate(ConsoleDeviceGetDefault,CONSOLE_POSITION_BOTTOM,False);
- ConsoleWindowSetBackColor(MessagesWindow,COLOR_WHITE);
- ConsoleClrScr;
- ScopeThread:=BeginThread(@ScopeThreadProcedure,nil,ScopeThread,THREAD_STACK_DEFAULT_SIZE);
- StartLogging;
- DiagnosticConsoleLog(Format('Ultibo Release %s %s %s',[ULTIBO_RELEASE_DATE,ULTIBO_RELEASE_NAME,ULTIBO_RELEASE_VERSION]));
- DiagnosticConsoleLog(Format('TSystemDiagnostic data starts at 0x%x',[DiagnosticPointer + 32]));
- DiagnosticConsoleLog('');
+  SystemDiagnostic := TSystemDiagnostic.Create(Pointer(DiagnosticPointer));
+  IdleThread := BeginThread(@IdleThreadProcedure,nil,IdleThread,THREAD_STACK_DEFAULT_SIZE);
+  ScopeWindow := ConsoleWindowCreate(ConsoleDeviceGetDefault, CONSOLE_POSITION_TOP, True);
+  ConsoleWindowSetBackColor(ScopeWindow, COLOR_ORANGE);
+  MessagesWindow := ConsoleWindowCreate(ConsoleDeviceGetDefault, CONSOLE_POSITION_BOTTOM, False);
+  ConsoleWindowSetBackColor(MessagesWindow, COLOR_WHITE);
+  ConsoleClrScr;
+  ScopeThread := BeginThread(@ScopeThreadProcedure,nil,ScopeThread,THREAD_STACK_DEFAULT_SIZE);
+  StartLogging;
+  DiagnosticConsoleLog(Format('Ultibo Release %s %s %s', [ULTIBO_RELEASE_DATE, ULTIBO_RELEASE_NAME, ULTIBO_RELEASE_VERSION]));
+  DiagnosticConsoleLog(Format('TSystemDiagnostic data starts at 0x%x', [DiagnosticPointer + 32]));
+  DiagnosticConsoleLog('');
 end;
 
 initialization
  InitDiagnosticConsole;
+
 end.
